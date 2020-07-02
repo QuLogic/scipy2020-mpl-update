2736f8b276316ef1a57d09871474526dbbfc2454
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-07-02 05:40:50 -0400

    Add a history timeline.

--- slides.py
+++ slides.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+import json
 import sys
+import urllib.request
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.cm as cm
+import matplotlib.dates as mdates
 import matplotlib.font_manager
 from matplotlib.patches import Rectangle, PathPatch
 from matplotlib.textpath import TextPath
@@ -102,8 +106,79 @@ def title(pdf):
     pdf.savefig(fig)
 
 
+def history(pdf):
+    fig = plt.figure(figsize=(19.2, 10.8), dpi=100)
+
+    fig.text(0.05, 0.8, 'Release History',
+             fontproperties=font, color='C0', fontsize=72)
+
+    try:
+        url = 'https://api.github.com/repos/matplotlib/matplotlib/releases'
+        url += '?per_page=100'
+        data = json.loads(
+            urllib.request.urlopen(url, timeout=.4).read().decode())
+
+        dates = []
+        names = []
+        for item in data:
+            if 'rc' not in item['tag_name'] and 'b' not in item['tag_name']:
+                dates.append(item['published_at'].split("T")[0])
+                names.append(item['tag_name'])
+        # Convert date strings (e.g. 2014-10-18) to datetime
+        dates = [datetime.strptime(d, "%Y-%m-%d") for d in dates]
+
+    except Exception:
+        # In case the above fails, e.g. because of missing internet connection
+        # use the following lists as fallback.
+        names = ['v2.2.4', 'v3.0.3', 'v3.0.2', 'v3.0.1', 'v3.0.0', 'v2.2.3',
+                 'v2.2.2', 'v2.2.1', 'v2.2.0', 'v2.1.2', 'v2.1.1', 'v2.1.0',
+                 'v2.0.2', 'v2.0.1', 'v2.0.0', 'v1.5.3', 'v1.5.2', 'v1.5.1',
+                 'v1.5.0', 'v1.4.3', 'v1.4.2', 'v1.4.1', 'v1.4.0']
+
+        dates = ['2019-02-26', '2019-02-26', '2018-11-10', '2018-11-10',
+                 '2018-09-18', '2018-08-10', '2018-03-17', '2018-03-16',
+                 '2018-03-06', '2018-01-18', '2017-12-10', '2017-10-07',
+                 '2017-05-10', '2017-05-02', '2017-01-17', '2016-09-09',
+                 '2016-07-03', '2016-01-10', '2015-10-29', '2015-02-16',
+                 '2014-10-26', '2014-10-18', '2014-08-26']
+
+        # Convert date strings (e.g. 2014-10-18) to datetime
+        dates = [datetime.strptime(d, "%Y-%m-%d") for d in dates]
+
+    levels = np.tile([-5, 5, -3, 3, -1, 1],
+                     int(np.ceil(len(dates) / 6)))[:len(dates)]
+
+    ax = fig.add_axes((0.05, 0.11, 0.9, 0.7))
+
+    ax.vlines(dates, 0, levels, color="tab:red")
+    ax.plot(dates, np.zeros_like(dates), "-o",
+            color="k", markerfacecolor="w")
+
+    # annotate lines
+    for d, l, r in zip(dates, levels, names):
+        ax.annotate(r, xy=(d, l),
+                    xytext=(-3, np.sign(l)*3), textcoords="offset points",
+                    horizontalalignment="right",
+                    verticalalignment="bottom" if l > 0 else "top")
+
+    # format xaxis with 4 month intervals
+    ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))
+    ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+
+    # remove y axis and spines
+    ax.get_yaxis().set_visible(False)
+    for spine in ["left", "top", "right"]:
+        ax.spines[spine].set_visible(False)
+
+    ax.margins(y=0.1)
+
+    pdf.savefig(fig)
+
+
 if 'Carlito' not in matplotlib.font_manager.findfont('Carlito:bold'):
     sys.exit('Carlito font must be installed.')
 font = matplotlib.font_manager.FontProperties(family='Carlito', weight='bold')
 with PdfPages('slides.pdf') as pdf:
     title(pdf)
+    history(pdf)

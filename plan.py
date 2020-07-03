d11d86560397c8b8128003f083badd9e2c31d86f
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2020-07-03 03:07:47 -0400

    Update future plans.

--- plan.py
+++ plan.py
@@ -4,13 +4,22 @@ from mplslide import BULLET, FONT, new_slide, slide_heading
 def slides():
     fig = new_slide()
 
-    slide_heading(fig, 'Release Plan')
+    slide_heading(fig, 'Future Plans')
 
-    fig.text(0.05, 0.6, 'Next feature release: 3.4',
-             fontproperties=FONT, alpha=0.7, fontsize=56)
-    fig.text(0.1, 0.5, f'{BULLET} September 2020',
-             fontproperties=FONT, alpha=0.7, fontsize=56)
-    fig.text(0.1, 0.4, f'{BULLET} Dropping Python 3.6 support',
-             fontproperties=FONT, alpha=0.7, fontsize=56)
+    props = dict(fontproperties=FONT, fontsize=56, alpha=0.7,
+                 verticalalignment='top')
+
+    fig.text(0.05, 0.8, 'Next feature release: 3.4', **props)
+    fig.text(0.1, 0.7, f'{BULLET} September 2020', **props)
+    fig.text(0.1, 0.6,
+             f'{BULLET} Dropping support for Python 3.6 & NumPy 1.15',
+             **props)
+
+    fig.text(0.05, 0.4, 'Google Season of Docs 2020', **props)
+
+    fig.text(0.05, 0.2, 'Check out our blog!', **props)
+    t = fig.text(0.1, 0.2, '\nhttps://matplotlib.org/matplotblog/',
+                 **props)
+    t.set_url('https://matplotlib.org/matplotblog/')
 
     return fig
